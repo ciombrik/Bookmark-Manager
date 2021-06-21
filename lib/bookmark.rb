44d2dcea15b01e11070251e5fbddd83c5c973342
Alex <alexandrachorbe@MA324.local>
2021-06-21 20:19:43 +0100

    viewing Bookmarks

---
+++ lib/bookmark.rb
@@ -0,0 +1,5 @@
+class Bookmark
+  def self.all
+    ["Google", "Makers Academy"]
+  end
+end
--- spec/bookmarks_spec.rb
+++ spec/bookmarks_spec.rb
@@ -1,6 +1,6 @@
 feature "viewing bookmarks" do
   scenario "page shows the list of bookmarks" do
-    visit ("/bookmarks/index")
+    visit ("/bookmarks")
     expect(page).to have_content "Google"
   end
 end
